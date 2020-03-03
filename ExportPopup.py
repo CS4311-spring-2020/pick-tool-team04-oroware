e01fdf4032a63ce6536cbf3de3b77888d04a4a79
mawilliams7 <mawilliams7@miners.utep.edu>
2020-03-03 11:38:37 -0700

    Bug fixes for demo

---
+++ ExportPopup.py
@@ -0,0 +1,70 @@
+from PyQt5 import QtCore
+from PyQt5.QtWidgets import *
+import csv
+
+
+class ExportPopup(QWidget):
+    def __init__(self, vectorName, vectorTableWidget, relationshipTableWidget, figure):
+        super(ExportPopup, self).__init__()
+        self.vectorName = vectorName
+        self.vectorTableWidget = vectorTableWidget
+        self.relationshipTableWidget = relationshipTableWidget
+        self.figure = figure
+        self.layout = QVBoxLayout()
+        self.locationLabel = QLabel()
+        self.locationLabel.setText("Location: ")
+        self.layout.addWidget(self.locationLabel)
+        self.fileDialog = QFileDialog()
+        self.fileDialog.setFileMode(QFileDialog.AnyFile)
+        self.layout.addWidget(self.fileDialog)
+        self.exportButton = QPushButton('Export', self)
+        self.exportButton.clicked.connect(self.onExportClick)
+        self.layout.addWidget(self.exportButton)
+        self.setLayout(self.layout)
+        self.setWindowTitle("Export Popup")
+
+    def exportVectorTable(self, exportPath):
+        filename = exportPath + "/" + self.vectorName + "_SignificantEventTable.csv"
+        with open(filename, 'w') as csv_file:
+            writer = csv.writer(csv_file, delimiter=",", lineterminator='\n')
+            model = self.vectorTableWidget.model()
+            row = list()
+            row.append("ID")
+            for col_num in range(model.columnCount()):
+                row.append(model.headerData(col_num, QtCore.Qt.Horizontal))
+            writer.writerow(row)
+            for row_num in range(model.rowCount()):
+                row = list()
+                row.append(str(model.headerData(row_num, QtCore.Qt.Vertical)))
+                for col_num in range(model.columnCount()):
+                    text = model.data(model.index(row_num, col_num))
+                    row.append(str(text))
+                writer.writerow(row)
+
+    def exportRelationshipTable(self, exportPath):
+        filename = exportPath + "/" + self.vectorName + "_RelationshipTable.csv"
+        with open(filename, 'w') as csv_file:
+            writer = csv.writer(csv_file, delimiter=",", lineterminator='\n')
+            model = self.relationshipTableWidget.model()
+            row = list()
+            row.append("ID")
+            for col_num in range(model.columnCount()):
+                row.append(model.headerData(col_num, QtCore.Qt.Horizontal))
+            writer.writerow(row)
+            for row_num in range(model.rowCount()):
+                row = list()
+                row.append(str(model.headerData(row_num, QtCore.Qt.Vertical)))
+                for col_num in range(model.columnCount()):
+                    text = model.data(model.index(row_num, col_num))
+                    row.append(str(text))
+                writer.writerow(row)
+
+    def exportGraph(self, exportPath):
+        self.figure.savefig((exportPath + "/" + self.vectorName + "_Graph.png"), format="PNG")
+
+    def onExportClick(self):
+        exportPath = self.fileDialog.selectedFiles()[0]
+        self.exportVectorTable(exportPath)
+        self.exportRelationshipTable(exportPath)
+        self.exportGraph(exportPath)
+        self.close()
